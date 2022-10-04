e055f7d61ea75a9c451e869c516ef445609313ac
Markus Ryöti <markus.ryoti@student.tut.fi>
2022-10-04 19:55:58 +0300

    refactored island count

--- islands/islands.py
+++ islands/islands.py
@@ -20,21 +20,21 @@ def num_islands(grid):
 
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-            if grid[i][j] == 'L' and stringify(i, j) not in visited:
-                bfs(grid, i, j, visited) 
+            is_new = bfs(grid, i, j, visited) 
+            if is_new:
                 num += 1
 
     return num
 
 def dfs(grid, i, j, visited):
     if off_map(grid, i, j):
-        return
+        return False
 
     if stringify(i, j) in visited:
-        return
+        return False
 
     if grid[i][j] == 'W':
-        return
+        return False
 
     visited[stringify(i, j)] = True
 
@@ -42,20 +42,31 @@ def dfs(grid, i, j, visited):
         new_i, new_j = i + d[0], j + d[1]
         dfs(grid, new_i, new_j, visited)
 
+    return True
+
 def bfs(grid, i, j, visited):
+    if stringify(i, j) in visited:
+        return False
+
+    if grid[i][j] == 'W':
+        return False
+
     q = [(i, j)]
 
     while len(q) > 0:
         curr = q.pop(0)
-        visited[stringify(*curr)] = True
 
         if grid[curr[0]][curr[1]] == 'W':
             continue
 
+        visited[stringify(*curr)] = True
+
         for d in directions:
             new_i, new_j = curr[0] + d[0], curr[1] + d[1]
-            if stringify(new_i, new_j) not in visited and not off_map(grid, new_i, new_j):
+            if not off_map(grid, new_i, new_j) and stringify(new_i, new_j) not in visited:
                 q.append((new_i, new_j))
+
+    return True
 
 def stringify(i, j):
     return str(i) + ',' + str(j)
