5b044a3bfe5667bc3049870073eb2ba74dc21ac5
Markus Ryöti <markus.ryoti@student.tut.fi>
2022-10-04 17:39:06 +0300

    shortest path refactor

--- shortest_path/shortest.py
+++ shortest_path/shortest.py
@@ -17,39 +17,32 @@ def build_graph(edges):
         a, b = e
 
         if a not in graph:
-            graph[a] = [b]
-        else:
-            graph[a].append(b)
-
+            graph[a] = []
         if b not in graph:
-            graph[b] = [a]
-        else:
-            graph[b].append(a)
+            graph[b] = []
+
+        graph[b].append(a)
+        graph[a].append(b)
 
     return graph
 
 def bfs(graph, start, dst):
-    shortest = 100
     visited = dict()
-
     q = [[start, 0]]
 
     while len(q) > 0:
         curr = q.pop(0)
-        curr_node, d_from_start = curr[0], curr[1]
+        curr_node, d_start = curr
         visited[curr_node] = True
 
         if curr_node == dst:
-            if d_from_start < shortest:
-                shortest = d_from_start
-
-        d_from_start += 1
+            return d_start
 
         for n in graph[curr_node]:
             if n not in visited:
-                q.append([n, d_from_start])
+                q.append([n, d_start + 1])
 
-    return shortest
+    return -1
 
 def main():
     shortest = shortest_path(edges, 'w', 'z')
